f3319bb3ba629add52d81846ef1c38839ceef1d7
mleisa <melcafidan@gmail.com>
2024-01-24 21:50:27 +0100

    Service-B

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "service-bus-module" {
+  source             = "git::https://github.com/mleisa/assignment.git"
+  namespace_name     = "namespace-aa"
+  queue_name_and_dlq = {
+    "queue2" = false
+  }
+  action_group_name = "team1"
+  alert_rule        = {
+    rule-a = {
+      alert_name         = "Service-B-Alert"
+      metric_name        = "Messages"
+      threshold          = 50
+      dimension_operator = "Include"
+      queue_name_list    = ["queue2"]
+    }
+  }
+}
