cf825cb4d1979524659533c9d37d63a6961d3f6f
mleisa <melcafidan@gmail.com>
2024-01-25 10:32:18 +0100

    Update variables

--- main.tf
+++ main.tf
@@ -16,12 +16,16 @@ module "service-bus-module" {
   source             = "git::https://github.com/mleisa/assignment.git"
   namespace_name     = "namespace-aa"
   queue_name_and_dlq = {
-    "queue2" = false
+    queue2 = {
+      queue_name             = "queue2"
+      dead_lettering_enabled = false
+    }
   }
   action_group_name = "team1"
+  action_group_short_name = "p1-action"
   alert_rule        = {
     rule-a = {
-      alert_name         = "Service-B-Alert"
+      alert_name         = "Service-B-Queue2"
       metric_name        = "Messages"
       threshold          = 50
       dimension_operator = "Include"
